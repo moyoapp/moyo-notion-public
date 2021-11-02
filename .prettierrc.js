fd4bb8510558cb11937b295307a6c0b78a85ea72
JunYoung Kim <sk990420@naver.com>
2021-11-03 03:14:02 +0900

    프로젝트 초기 설정 : styled-components, styled-normalize 설치 및 ssr 설정

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxBrackets: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 80,
+};
---
+++ pages/_document.tsx
@@ -0,0 +1,48 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const page = ctx.renderPage(
+      (App) => (props) =>
+        sheet.collectStyles(
+          <>
+            <Normalize />
+            <App {...props} />
+          </>,
+        ),
+    );
+    const styles = sheet.getStyleElement();
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps, ...page, styles };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <Head>
+            <title>모여</title>
+            <meta name="description" content="모여. 모임과 공간을 연결하다." />
+            <link rel="icon" href="/favicon.ico" />
+            {this.props.styles}
+          </Head>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
